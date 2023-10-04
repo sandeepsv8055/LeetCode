397742983e71094328725cabb84386ea5d0661a3
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2023-10-04 21:38:23 +0530

    Add files via upload

---
+++ vector......cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+# include <vector>
+using namespace std;
+int main (){
+
+	int size;									// number of the  elements in the vector 
+	cin>>size;
+	int arr[size];                     
+	for (int i =0;i<size;i++){
+		cin>>arr[i];
+	}
+	
+	
+	
+	for (int i =0;i<v.size();i++){
+		cout<<arr[i]<<" ";
+	}
+}
