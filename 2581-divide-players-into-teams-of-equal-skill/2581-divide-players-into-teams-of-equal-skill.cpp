9802d1e05095ee46fd3f9525ba167841f699af80
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2024-10-06 19:36:31 +0530

    Time: 55 ms (81.41%), Space: 58.2 MB (8.75%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -1,27 +1,56 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& arr) {
-        int n= arr.size();
+        int n = arr.size();
         sort(arr.begin(),arr.end());
+        int x = arr[0] + arr[n-1];
         vector<int>ans;
-        int x = arr[0]+arr[n-1];
-        int i=0;
-        while(i<n/2){
-            int x1 = arr[i]+arr[n-i-1];
-            if(x1!=x)
-            {
+        for(int i=0;i<n/2;i++){
+            int y = arr[i] + arr[n-i-1];
+            if(x!=y){
                 return -1;
             }
-            else {
-               ans.push_back(arr[i]*arr[n-i-1]) ;
-            }
-            i++;
-
+            else 
+                 ans.push_back(arr[i] * arr[n-i-1]);
         }
-        long long sum=0;
+        long long sum =0;
         for(int i=0;i<ans.size();i++){
-             sum +=ans[i];
+            sum += ans[i];
         }
         return sum;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+        // int n= arr.size();
+        // sort(arr.begin(),arr.end());
+        // vector<int>ans;
+        // int x = arr[0]+arr[n-1];
+        // int i=0;
+        // while(i<n/2){
+        //     int x1 = arr[i]+arr[n-i-1];
+        //     if(x1!=x)
+        //     {
+        //         return -1;
+        //     }
+        //     else {
+        //        ans.push_back(arr[i]*arr[n-i-1]) ;
+        //     }
+        //     i++;
+        // }
+        // int sum=0;
+        // for(int i=0;i<ans.size();i++){
+        //      sum +=ans[i];
+        // }
+        // return sum;
+   // }
+//};
