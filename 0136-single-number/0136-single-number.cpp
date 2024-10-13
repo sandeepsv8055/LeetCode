176da653dd324274cf7118801b6fbaedf760246d
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2024-10-13 22:15:27 +0530

    Time: 586 ms (5.04%), Space: 19.5 MB (86.82%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,21 +1,62 @@
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
+// class Solution {
+// public:
+//     int singleNumber(vector<int>& nums) {
+        //approach 1 XOR ^
     //     int ans =nums[0];
     // for(int i=1;i<nums.size();i++){
-    //         ans^=nums[i];
+    //         ans^=nums[i]; // XOR each number
     //     }
     //     int a=2^2;
     //     cout<<a<<endl;
     //     return ans;
-    map<int,int>mp;
-    for(int i=0;i<nums.size();i++){
-        mp[nums[i]]++;
-    }
-    for(int i=0;i<nums.size();i++){
-        if(mp[nums[i]]>1) continue;
-        else return  nums[i];
-    }
-    return nums[0];
+
+    // approach 2
+    // map<int,int>mp;
+    // for(int i=0;i<nums.size();i++){
+    //     mp[nums[i]]++;
+    // }
+    // for(int i=0;i<nums.size();i++){
+    //     if(mp[nums[i]]>1) continue;
+    //     else return  nums[i];
+    // }
+    // return nums[0];
+    
+    //approach 3 sort
+    // sort(nums.begin(),nums.end());
+    // int cnt=0;
+    // for(int i=0;i<nums.size();i++){
+    //     if(nums[i]==nums[i+1]) cnt++;
+    //     if(cnt ==1)
+    // }
+
+
+//}
+//};
+
+
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // Brute force approach
+        for (int i = 0; i < nums.size(); i++) {
+            int count = 0; // Reset count for each new element
+            
+            // Check how many times nums[i] appears in the array
+            for (int j = 0; j < nums.size(); j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+            
+            // If count is 1, we found the single number
+            if (count == 1) {
+                return nums[i];
+            }
+        }
+
+        // Return -1 if no single number is found (just for safety, should never reach here)
+        return -1;
     }
 };
