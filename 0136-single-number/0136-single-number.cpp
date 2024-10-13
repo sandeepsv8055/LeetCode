290f83ee9a4f9d88d07b56bc484eed0416ad26e9
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2024-10-13 20:12:52 +0530

    Time: 44 ms (5.04%), Space: 24.6 MB (5.54%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans =nums[0];
-    for(int i=1;i<nums.size();i++){
-            ans^=nums[i];
-        }
-        return ans;
+    //     int ans =nums[0];
+    // for(int i=1;i<nums.size();i++){
+    //         ans^=nums[i];
+    //     }
+    //     int a=2^2;
+    //     cout<<a<<endl;
+    //     return ans;
+    map<int,int>mp;
+    for(int i=0;i<nums.size();i++){
+        mp[nums[i]]++;
+    }
+    for(int i=0;i<nums.size();i++){
+        if(mp[nums[i]]>1) continue;
+        else return  nums[i];
+    }
+    return nums[0];
     }
 };
