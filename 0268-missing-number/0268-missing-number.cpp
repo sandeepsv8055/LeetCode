779447ac90603a64665f3a04ca7fd1bfba63f563
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2024-10-15 09:26:53 +0530

    Time: 14 ms (55.86%), Space: 20.7 MB (37.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,17 +1,31 @@
+// Approcah 1
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         int n = nums.size();
+//         sort(nums.begin(), nums.end()); 
+
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] != i) {
+//                 return i; // Return the first index where nums[i] != i
+//             }
+//         }
+
+//         // If all numbers are present, the missing number is 'n'
+//         return n;
+//     }
+// };
+
+//apporcah 2
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(), nums.end()); 
-
-        // Check if the missing number is in the middle
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != i) {
-                return i; // Return the first index where nums[i] != i
-            }
+        int expectedSum = n*(n+1)/2;
+        int currentSum = 0;
+        for(int i=0; i<n;i++){
+            currentSum += nums[i];
         }
-
-        // If all numbers are present, the missing number is 'n'
-        return n;
+        return expectedSum - currentSum;
     }
 };
