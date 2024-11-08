b094fcf7a536d7c85b1b53afdc4f0d4a976dab37
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2024-11-08 23:48:28 +0530

    Time: 0 ms (100%), Space: 14.7 MB (34.37%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,28 +8,58 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+//better
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int len =0;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            len++;
-            temp = temp->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        // traverse fast to n+1 steps
+        for(int i=1;i<=n+1;i++){
+            if(fast == NULL) return head->next;
+            fast = fast->next;
         }
-        if(n == len){
-            head = head->next;
-            return head;
+        //move slow and fast at same pace
+        while(fast!=NULL){
+            slow = slow->next;
+            fast = fast->next;
         }
-        //nth node from end = (len-n+1) from start
-        int m = len - n + 1 ;
-        int idx = m-1; //the idx of node to be delted
-        temp = head;
-        for(int i=1;i<=idx-1;i++){
-            temp = temp->next;
-        }
-        temp->next = temp ->next->next;
+        //now slow is the exactly the node od left which to be dlted
+        slow->next = slow->next->next;
         return head;
+
 
     }
 };
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         int len =0;
+//         ListNode* temp = head;
+//         while(temp!=NULL){
+//             len++;
+//             temp = temp->next;
+//         }
+//         if(n == len){
+//             head = head->next;
+//             return head;
+//         }
+//         //nth node from end = (len-n+1) from start
+//         int m = len - n + 1 ;
+//         int idx = m-1; //the idx of node to be delted
+//         temp = head;
+//         for(int i=1;i<=idx-1;i++){
+//             temp = temp->next;
+//         }
+//         temp->next = temp ->next->next;
+//         return head;
+
+//     }
+// };
