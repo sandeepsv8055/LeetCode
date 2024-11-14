1fe54619750f9ffe4b138322012865d063c6b051
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2024-11-14 18:40:12 +0530

    Time: 16 ms (10.95%), Space: 15.1 MB (9.19%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -6,17 +6,36 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         ListNode *temp = head;
+//         ListNode * slow = head ;
+//         ListNode * fast = head;
+//         while(fast!=NULL && fast->next!=NULL){
+//             slow = slow->next;
+//             fast = fast->next->next;
+//             if(slow==fast) return true;
+//         }
+//         return false;
+//     }
+// };
+
+//using hashmap
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        unordered_map<ListNode *,bool> visited;
         ListNode *temp = head;
-        ListNode * slow = head ;
-        ListNode * fast = head;
-        while(fast!=NULL && fast->next!=NULL){
-            slow = slow->next;
-            fast = fast->next->next;
-            if(slow==fast) return true;
+        while(temp!=NULL){
+            //check if current node is already i the map
+            if(visited.find(temp)!=visited.end()){
+                return true;
+            }
+            //mark the current node as visited
+            visited[temp] = true;
+            temp=temp->next;
         }
-        return false;
+        return false;// no cycle
     }
 };
