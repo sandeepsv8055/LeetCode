1fac5660e274b0f4070fe31b4f28257b714ca202
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2024-11-17 22:25:03 +0530

    Time: 0 ms (100%), Space: 19.8 MB (59.58%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -8,37 +8,67 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- // SC = O(m+n  )
+
+// O(1) space
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
-        ListNode* tempA = a;
-        ListNode* tempB = b;
         ListNode* c = new ListNode(100);
-        ListNode* tempC = c;
-        while(tempA!=NULL && tempB!=NULL){
-            if(tempA->val <= tempB->val){
-                //creating new node
-                ListNode* t = new ListNode(tempA->val);
-                tempC->next = t;
-                tempC = t;
-                tempA = tempA->next;
-            }
-            else{// tempB ki val badi h
-                //creating new node
-                ListNode* t = new ListNode(tempB->val);
-                tempC->next = t;
-                tempC = t;
-                tempB = tempB->next;
-            }
+        ListNode* temp = c;
+        while(a!=NULL && b!=NULL){
+        if(a->val <= b->val){
+            temp->next = a;
+            a = a->next;
+            temp = temp->next;
         }
-        if(tempA == NULL){
-            tempC->next = tempB;
+        else{ //b is less than a
+            temp->next = b;
+            b = b->next;
+            temp = temp->next;
         }
-        else { //tempB==NULL
-            tempC->next = tempA;
+        }
+        if(a==NULL){
+            temp->next = b;
+        }
+        else{
+            temp->next = a;
         }
         return c->next;
-            
     }
 };
+
+
+//  // SC = O(m+n  )
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+//         ListNode* tempA = a;
+//         ListNode* tempB = b;
+//         ListNode* c = new ListNode(100);
+//         ListNode* tempC = c;
+//         while(tempA!=NULL && tempB!=NULL){
+//             if(tempA->val <= tempB->val){
+//                 //creating new node
+//                 ListNode* t = new ListNode(tempA->val);
+//                 tempC->next = t;
+//                 tempC = t;
+//                 tempA = tempA->next;
+//             }
+//             else{// tempB ki val badi h
+//                 //creating new node
+//                 ListNode* t = new ListNode(tempB->val);
+//                 tempC->next = t;
+//                 tempC = t;
+//                 tempB = tempB->next;
+//             }
+//         }
+//         if(tempA == NULL){
+//             tempC->next = tempB;
+//         }
+//         else { //tempB==NULL
+//             tempC->next = tempA;
+//         }
+//         return c->next;
+            
+//     }
+// };
