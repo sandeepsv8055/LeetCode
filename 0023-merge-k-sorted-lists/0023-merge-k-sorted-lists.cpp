1f2a37cec690527f5b2645cc6125c6cede16dd5f
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2024-11-19 18:43:37 +0530

    Time: 23 ms (21.8%), Space: 18.4 MB (50.46%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,8 +8,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
- //Approach 1 bekar
+// Approach-2
 class Solution {
 public:
 
@@ -40,13 +39,61 @@ public:
     ListNode* mergeKLists(vector<ListNode*>& arr) {
         if(arr.size()==0) return NULL;
         while(arr.size()>1){
-            ListNode* a = arr[arr.size()-1];
-            arr.pop_back();
-            ListNode* b = arr[arr.size()-1];
-            arr.pop_back();
+            ListNode* a = arr[0];
+            arr.erase(arr.begin());
+            ListNode* b = arr[0];
+            arr.erase(arr.begin());
             ListNode* c = merge(a,b);
             arr.push_back(c);
         }
         return arr[0];
     }
 };
+
+
+
+
+
+
+
+ //Approach 1 bekar
+// class Solution {
+// public:
+
+//     ListNode* merge(ListNode* a, ListNode* b) {
+//         ListNode* c = new ListNode(100);
+//         ListNode* temp = c;
+//         while(a!=NULL && b!=NULL){
+//         if(a->val <= b->val){
+//             temp->next = a;
+//             a = a->next;
+//             temp = temp->next;
+//         }
+//         else{ //b is less than a
+//             temp->next = b;
+//             b = b->next;
+//             temp = temp->next;
+//         }
+//         }
+//         if(a==NULL){
+//             temp->next = b;
+//         }
+//         else{
+//             temp->next = a;
+//         }
+//         return c->next;
+//     }
+
+//     ListNode* mergeKLists(vector<ListNode*>& arr) {
+//         if(arr.size()==0) return NULL;
+//         while(arr.size()>1){
+//             ListNode* a = arr[arr.size()-1];
+//             arr.pop_back();
+//             ListNode* b = arr[arr.size()-1];
+//             arr.pop_back();
+//             ListNode* c = merge(a,b);
+//             arr.push_back(c);
+//         }
+//         return arr[0];
+//     }
+// };
