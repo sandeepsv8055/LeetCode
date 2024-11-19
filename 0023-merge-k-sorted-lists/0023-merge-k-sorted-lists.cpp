33d9770b220520f7a9629d698dca9ebd118c8b74
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2024-11-19 18:22:28 +0530

    Time: 109 ms (12.35%), Space: 18.5 MB (50.46%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,23 +8,45 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+ //Approach 1 bekar
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int> v1;
-        for (auto list : lists) {
-            while (list) {
-                v1.push_back(list->val);
-                list = list->next;
-            }
-        }
-        sort(v1.begin(), v1.end());
-        ListNode* dummy = new ListNode(0);
-        ListNode* temp = dummy;
-        for (auto i : v1) {
-            temp->next = new ListNode(i);
+
+    ListNode* merge(ListNode* a, ListNode* b) {
+        ListNode* c = new ListNode(100);
+        ListNode* temp = c;
+        while(a!=NULL && b!=NULL){
+        if(a->val <= b->val){
+            temp->next = a;
+            a = a->next;
             temp = temp->next;
         }
-        return dummy->next;
+        else{ //b is less than a
+            temp->next = b;
+            b = b->next;
+            temp = temp->next;
+        }
+        }
+        if(a==NULL){
+            temp->next = b;
+        }
+        else{
+            temp->next = a;
+        }
+        return c->next;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& arr) {
+        if(arr.size()==0) return NULL;
+        while(arr.size()>1){
+            ListNode* a = arr[arr.size()-1];
+            arr.pop_back();
+            ListNode* b = arr[arr.size()-1];
+            arr.pop_back();
+            ListNode* c = merge(a,b);
+            arr.push_back(c);
+        }
+        return arr[0];
     }
 };
