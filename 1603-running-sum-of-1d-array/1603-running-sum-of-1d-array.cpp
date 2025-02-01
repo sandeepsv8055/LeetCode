12d0d60342724d93fa33d93cf856753b907ba612
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2025-02-01 11:09:42 +0530

    Time: 0 ms (100%), Space: 12.6 MB (50.79%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.cpp
@@ -14,11 +14,14 @@ public:
 
         //single pass
 
-        int n = nums.size();
-        for(int i =1;i<n;i++){
-            // nums[i] = nums[i]+nums[i-1];
-            nums[i] += nums[i-1];
-        }
+        // int n = nums.size();
+        // for(int i =1;i<n;i++){
+        //     // nums[i] = nums[i]+nums[i-1];
+        //     nums[i] += nums[i-1];
+        // }
+        // return nums;
+
+        for(int i=1;i<nums.size();i++) nums[i] += nums[i-1];
         return nums;
 
      
