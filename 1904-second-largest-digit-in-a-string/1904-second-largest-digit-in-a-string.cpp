49fb4799a660895f8ee8b3e9bc096b3754c5dc77
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2025-07-15 23:53:53 +0530

    Time: 0 ms (100%), Space: 9.5 MB (51.98%) - LeetHub

---
+++ 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int secondHighest(string s) {
+        map<int,int>mp;
+        for(int i=0;i<s.size();i++){
+            if(isdigit(s[i])){
+            mp[s[i]-'0']++;
+            }
+        }
+        if(mp.size()<=1){
+            return -1;
+        }
+        int cnt=0;
+        for (auto it = mp.rbegin(); it != mp.rend(); ++it) {
+            if(cnt==1){
+                return (*it).first;
+            }
+             cnt++;
+        }
+        return -1;
+    }
+};
