4eb09b2bf4bbd8ccabec78cbb3e9c16978075abd
sandeepsv8055 <143858545+sandeepsv8055@users.noreply.github.com>
2026-08-12 00:33:24 +0530

    Time: 0 ms (100%), Space: 22.7 MB (53.6%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,13 +1,63 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i =0;
-        int k = nums.size();
-        for(int j=1;j<k;j++){
-            if(nums[j]!=nums[i])
-            i++;
-            nums[i]=nums[j];
+         int n = nums.size();
+// //      set
+//         set<int>st;
+//         for(int i=0;i<n;i++){
+//             st.insert(nums[i]);
+//         }
+//         int index =0;
+//         for(auto it: st){
+//             nums[index] = it;
+//             index++;
+//         }
+//         return index;
+
+
+        //two pointer
+        int i=0;
+        for(int j=1;j<n;j++){
+            if(nums[j]!=nums[i]){
+                i++;
+                nums[i]=nums[j];
+            }
         }
         return i+1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int i=0;
+        // for(int j =i+1;j<n;j++){
+        //     if(nums[i]!=nums[j]){
+        //         i++;
+        //         nums[i]=nums[j];
+        //     }
+        // }
+        // return i+1;
+
+        // map<int, int> mp;
+        // for (int num : nums) {
+        //     mp[num]++;
+        // }
+
+        // // Overwrite the front of nums with unique elements
+        // int i = 0;
+        // for (auto& p : mp) {
+        //     nums[i++] = p.first;
+        // }
+
+        // return mp.size();
     }
 };
